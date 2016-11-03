39a9ce80081053284abf58c2a546a7924756ece5
Letitia She <wehuin1215@gmail.com>
2016-11-03 18:07:06 +0800

    Other specs done

---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'minitest/autorun'
+require 'minitest/rg'
+require 'rack/test'
+require 'vcr'
+require 'webmock'
+
+require_relative '../app'
+
+include Rack::Test::Methods
+
+def app
+  SpotifySearchAPI
+end
+
+FIXTURES_FOLDER = 'spec/fixtures'
+CASSETTES_FOLDER = "#{FIXTURES_FOLDER}/cassettes"
+GROUPS_CASSETTE = 'groups' #remember to change the string
+
+VCR.configure do |c|
+  c.cassette_library_dir = CASSETTES_FOLDER
+  c.hook_into :webmock
+end
