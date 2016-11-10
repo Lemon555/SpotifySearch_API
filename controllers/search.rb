d9f9ad104eeebe65fa787d6158dec9c61369fff3
Letitia She <wehuin1215@gmail.com>
2016-11-10 19:22:42 +0800

    POST fixed

--- controllers/search.rb
+++ controllers/search.rb
@@ -57,24 +57,19 @@ class SpotifySearchAPI < Sinatra::Base
 
   post "/#{API_VER}/:song_name/?" do
     track_name = params[:song_name]
-
-    if Group.find(input: track_name)
-      halt 422, "Search (input: #{track_name})already exists"
-    end
-
     begin
-      search = Spotify::Search.find(track_name)
-
-      content_type 'application/json'
-      result.to_json
+      if Group.find(input: track_name)
+        halt 422, "Search (input: #{track_name})already exists"
+      end
+      spotify_search = Spotify::Search.find(track_name)
     rescue
-      halt 404, "No (id: #{track_name}) found on Spotify"
+      halt 400, "No (#{track_name}) found on Spotify"
     end
 
     begin
       search = Search.create(input: track_name)
 
-      search.each do |key, track|
+      spotify_search.each do |key, track|
         Song.create(
           track_id: key,
           search_id: search.id,
