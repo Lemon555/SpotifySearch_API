e9735027c9f73081f41a81e522023d468ecebd7c
Elisa Wang <elisa.wang@iss.nthu.edu.tw>
2016-11-11 10:14:26 +0800

    Fixed search_spec.rb

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -17,8 +17,7 @@ describe 'search specifications' do
     end
 
     it '(HAPPY) should load and save a new result of search by given input' do
-      post 'api/v0.1',
-           { song_name: HAPPY_SEARCH_INPUT }.to_json,
+      post "api/v0.1/#{HAPPY_SEARCH_INPUT}".to_json,
            'CONTENT_TYPE' => 'application/json'
 
       last_response.status.must_equal 200
@@ -31,8 +30,7 @@ describe 'search specifications' do
     end
 
     it '(BAD) should report error if given invalid input' do
-      post 'api/v0.1',
-           { song_name: SAD_SEARCH_INPUT }.to_json,
+      post "api/v0.1/#{SAD_SEARCH_INPUT}".to_json,
            'CONTENT_TYPE' => 'application/json'
 
       last_response.status.must_equal 400
@@ -41,8 +39,7 @@ describe 'search specifications' do
 
     it 'should report error if searching keyword already exists' do
       2.times do
-        post 'api/v0.1',
-             { song_name: HAPPY_SEARCH_INPUT }.to_json,
+        post "api/v0.1/#{HAPPY_SEARCH_INPUT}".to_json,
              'CONTENT_TYPE' => 'application/json'
       end
 
@@ -50,12 +47,38 @@ describe 'search specifications' do
     end
   end
 
+  describe 'Do a search' do
+      it 'HAPPY: should find a array of album given a track name' do
+        get "api/v0.1/#{HAPPY_SEARCH_INPUT}"
+        last_response.status.must_equal 200
+        last_response.content_type.must_equal 'application/json'
+        search_data = JSON.parse(last_response.body)
+        search_data.is_a?(Array)
+        expect(search_data).not_to be_empty
+      end
+
+      it 'HAPPY: should find hash of images given a track name' do
+      type = 'images'
+      get "api/v0.1/#{HAPPY_SEARCH_INPUT}"
+
+      last_response.status.must_equal 200
+      last_response.content_type.must_equal 'application/json'
+      search_data = JSON.parse(last_response.body)
+      search_data['images'].is_a?(Hash)
+      end
+
+      it 'SAD: should report if no albums are found' do
+        get "api/v0.1/#{SAD_SEARCH_INPUT}"
+        search_data = JSON.parse(last_response.body)
+        expect(search_data).to be_empty
+      end
+  end
+
   describe 'Request to update a search' do
     after do
       DB[:searchs].delete
       DB[:songs].delete
-      post 'api/v0.1',
-           { song_name: HAPPY_SEARCH_INPUT }.to_json,
+      post "api/v0.1/#{HAPPY_SEARCH_INPUT}".to_json,
            'CONTENT_TYPE' => 'application/json'
     end
 
